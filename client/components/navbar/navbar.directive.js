f9bd13e2fa1a5df9b68610a9f6bda4b6b6906b4e
Juan David Martinez Rios <jdmartinez859@misena.edu.co>
2017-12-04 14:52:41 -0500

    se suben los archivos al repositorio remoto

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('parqueaderosApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
